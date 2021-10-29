f3395b4ef00be55ff6901c808217609fb45121b5
Atahanak <aakyildiz@sabanciuniv.edu>
2021-10-29 11:24:59 +0300

    fixed ignore, added reader

--- src/Tensor.hpp
+++ src/Tensor.hpp
@@ -1,6 +1,8 @@
 #ifndef _TENSOR_HPP
 #define _TENSOR_HPP
 
+#include <fstream>
+
 using namespace std;
 
 namespace sparsebase{
@@ -11,6 +13,7 @@ namespace sparsebase{
       virtual int get_rank() = 0;
   };
 
+
   class CSF : public Tensor{
     public:
       CSF(): Tensor() {cout<<"CSF created."<< endl;}
@@ -19,6 +22,7 @@ namespace sparsebase{
       virtual int get_rank(){return 0;}
   };
 
+  //template<typename i_t, typename c_t>
   class COO : public Tensor{
     public:
       COO(): Tensor() {cout<<"COO created."<< endl;}
@@ -34,11 +38,26 @@ namespace sparsebase{
         this->t = t;
       };
       ~TensorCreator(){};
+
       Tensor * create(){
         if(t == 0) return new CSF();
         else if(t == 1) return new COO();
         else return new COO();
       }
+  };
+
+  class TensorReader{
+    public:
+      virtual ~TensorReader(){};
+  };
+
+  template<typename i_t, typename nnz_t>
+  class MtxReader : public TensorReader{
+    public:
+      MtxReader(string filename): TensorReader(), filename(filename) {}
+    private:
+      string filename;
+      virtual ~MtxReader(){};
   };
 
 }
