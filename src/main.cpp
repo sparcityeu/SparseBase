e96daa1a78dd13ad23862bc618fc3b9b5f9632f6
Atahanak <aakyildiz@sabanciuniv.edu>
2021-10-30 14:51:11 +0000

    mtx reader for csr implemented

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,14 @@ using namespace sparsebase;
 
 int main(int argc, char * argv[]){
   cout << "F t re  s sp r e!" << endl;
-  Tensor * csf = new CSF(); 
-  Tensor * coo = new COO(); 
-  COO * coo2 = new COO(); 
+  Tensor * csf = new CSF(3); 
+  Tensor * csr = new CSR<unsigned int, unsigned int>(); 
+  Tensor * coo = new COO(3); 
+  COO * coo2 = new COO(3); 
   coo->get_rank(); 
-  coo2->go_crazy(); 
   SparseObject * graph = new Graph<int, int>();
+  string fname = "/data/GE/graphs/uedgelist/com-dblp_c.graph";
+  Graph<int, int> * graph2 = new Graph<int, int>(fname);
   
   return 0;
 }
