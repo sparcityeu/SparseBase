575ec6f642d5e982095de50317ed7be00776ee3e
AmroAlJundi <amr.jundi@gmail.com>
2021-11-04 11:57:05 +0300

    Desig 4 is working (partially)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "Tensor.hpp"
+#include "SparseFormat.hpp"
 #include "SparseObject.hpp"
 
 using namespace std;
@@ -8,14 +8,18 @@ using namespace sparsebase;
 
 int main(int argc, char * argv[]){
   cout << "F t re  s sp r e!" << endl;
-  Tensor * csf = new CSF(3); 
-  Tensor * csr = new CSR<unsigned int, unsigned int>(); 
-  Tensor * coo = new COO(3); 
-  COO * coo2 = new COO(3); 
-  coo->get_rank(); 
-  SparseObject * graph = new Graph<int, int>();
-  string fname = "/data/GE/graphs/uedgelist/com-dblp_c.graph";
-  Graph<int, int> * graph2 = new Graph<int, int>(fname);
+  unsigned int xadj[4] = {0, 2, 3, 4};
+  unsigned int adj[4] = {1,2,0,0};
+  CSR<unsigned int, unsigned int, unsigned int> csr(3, 3, xadj, adj, nullptr);
+  Graph<unsigned int, unsigned int> g(&csr);
+  //Tensor * csf = new CSF(3); 
+  //Tensor * csr = new CSR<unsigned int, unsigned int>(); 
+  //Tensor * coo = new COO(3); 
+  //COO * coo2 = new COO(3); 
+  //coo->get_rank(); 
+  //SparseObject * graph = new Graph<int, int>();
+  //string fname = "/data/GE/graphs/uedgelist/com-dblp_c.graph";
+  //Graph<int, int> * graph2 = new Graph<int, int>(fname);
   
   return 0;
 }
