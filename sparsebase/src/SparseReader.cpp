eeb58a07cf0c993f227d825596b3561ac3f05e34
ardasener <ardasenercs@gmail.com>
2021-12-08 20:11:50 +0300

    code comments are no longer stripped

--- sparsebase/src/SparseReader.cpp
+++ sparsebase/src/SparseReader.cpp
@@ -12,6 +12,13 @@ namespace sparsebase
   SparseReader<ID_t, NNZ_t>::~SparseReader(){};
 
   // Add weighted option with contexpr
+  //! Brief description
+  /*!
+    Detailed description
+    \param filename string
+    \param _weighted bool
+    \return vector of formats
+  */
   template <typename v_t, typename e_t, typename w_t>
   UedgelistReader<v_t, e_t, w_t>::UedgelistReader(string filename, bool _weighted) : filename(filename), weighted(_weighted) {}
   template <typename v_t, typename e_t, typename w_t>
