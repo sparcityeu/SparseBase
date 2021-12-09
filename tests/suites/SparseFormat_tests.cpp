befded992f89fc24900424dc48bcef6110de790c
AmroAlJundi <amr.jundi@gmail.com>
2021-12-09 21:29:37 +0300

    Restructured tests directory. Added a testing guide to the doxygen docs. Updated readme with fixed testing instructions

--- tests/suites/SparseFormat_tests.cpp
+++ tests/suites/SparseFormat_tests.cpp
@@ -1,8 +1,9 @@
+
 #include "gtest/gtest.h"
 
 #include "sparsebase/SparseFormat.hpp"
 
-TEST(FormatSuite, CSRFromPointers){
+TEST(SparseFormat, CreationTest){
   unsigned int xadj[4] = {0, 2, 3, 4};
   unsigned int adj[4] = {1,2,0,0};
   sparsebase::CSR<unsigned int, unsigned int, void> csr(3, 3, xadj, adj, nullptr);
