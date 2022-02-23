b610dfa6fd488ffc14cc4e5450910c6e666ff17e
AmroAlJundi <amr.jundi@gmail.com>
2022-02-23 16:04:15 +0300

    Added comments on how to specify data types for explicit instantiation

--- README.md
+++ README.md
@@ -40,6 +40,22 @@ This will generate the library as a static library. In addition, the example cod
 
 Due to optimizations and templates, this process might take several minutes.
 
+### Specifying explicit instantiation types
+Most classes in the library are templated over four template parameter types:
+
+1. `IDType`: data type that will contain IDs of element in a format object.
+2. `NNZType`: data type that will contain numbers of non-zeros in a format object. 
+3. `ValueType`: data type that will contain values stored inside format objects.
+4. `FloatType`: data type that will contain floating point values (e.g., degree distributions).
+
+When generating the build system, you can specify a list of data types for each one of these template parameters. Explicit instantiations of all the classes in the library will be created to satisfy the power set of all the types you passed. You can specify the data types using CMake cache variables that you pass as semi-colon-seperated, quotation-enclosed lists. For example, the following command:
+```bash
+cmake -DID_TYPES="usigned int; unsigned long long" -DNNZ_TYPES="unsigned long long; short" -DVALUE_TYPES="float" -DFLOAT_TYPES="double; float" ..
+```
+specifies the data types to be used with each of the four template parameter types stated above. 
+
+Alternatively, you can edit the `CMakeCache.txt` file located in the build directory. Note that this file is generated after creating the build system (after executing `cmake`).
+
 ### Header-only
 
 Additionally, the library has a header-only setting, in which none of the classes of the library will be explicitly instantiated at library-build time. Building the library to be header-only can be done as shown:
--- src/generate_explicit_instantiations.py
+++ src/generate_explicit_instantiations.py
@@ -10,6 +10,9 @@ parser.add_argument('--float-types', nargs='+', type=str, help= "C++ data types 
 parser.add_argument('--output-folder', type=str, help= "Path to output folder to store resultant files")
 args = parser.parse_args()
 
+'''
+Global variables defining data types
+'''
 vertex_types = ' '.join(args.id_types).split(',')
 nnz_types = ' '.join(args.nnz_types).split(',')
 value_types = ' '.join(args.value_types).split(',')
@@ -19,6 +22,9 @@ output_folder = args.output_folder
 PREFIX = "template class "
 temp_suffix = '.tmp'
 
+## Utility function that prints explicit instantiations of classes that require exactly three template parameters: IDType, NNZType, and ValueType
+# @param implementations: a list of strings corresponding to class names
+# @param out_stream: an output stream to which the instantiations will be printed
 def print_implementations(implementations, out_stream):
     for implementation_class in implementations:
         for vertex_type in vertex_types:
@@ -28,6 +34,7 @@ def print_implementations(implementations, out_stream):
         out_stream.write('\n\n\n')
 
 
+## Prints explicit template instantiations for the preprocess file
 def preprocess_cc_init():
     filename = os.path.join(output_folder,'preprocess.inc'+temp_suffix)
     with open(filename, 'w') as fout:
@@ -43,38 +50,47 @@ def preprocess_cc_init():
                         fout.write(PREFIX+"DegreeDistribution<"+vertex_type+", "+nnz_type+", "+value_type+", "+dist_type+">;\n")
         print_implementations(['ReorderPreprocessType', 'GenericReorder', 'DegreeReorder', 'RCMReorder', 'TransformPreprocessType', 'Transform'], fout)
     return filename
+
+## Prints explicit template instantiations for the converter file
 def converter_cc_init():
     filename = os.path.join(output_folder,'converter.inc'+temp_suffix)
     with open(filename, 'w') as fout:
         print_implementations(['Converter', 'CsrCooFunctor', 'CooCsrFunctor'], fout)
     return filename
 
+## Prints explicit template instantiations for the object file
 def object_cc_init():
     filename = os.path.join(output_folder,'object.inc'+temp_suffix)
     with open(filename, 'w') as fout:
         print_implementations(['AbstractObject', 'Graph'], fout)
     return filename
 
+## Prints explicit template instantiations for the format file
 def format_cc_init():
     filename = os.path.join(output_folder,'format.inc'+temp_suffix)
     with open(filename, 'w') as fout:
         print_implementations(['CSR', 'COO'], fout)
     return filename
 
+## Prints explicit template instantiations for the reader file
 def reader_cc_init():
     filename = os.path.join(output_folder,'reader.inc'+temp_suffix)
     with open(filename, 'w') as fout:
         print_implementations(['MTXReader', 'UedgelistReader'], fout)
     return filename
 
+## Create the output folder if it doesn't already exist
 if not os.path.isdir(output_folder):
     os.mkdir(output_folder)
+## Create temporary files containing the explicit instantiations
 files_created = []
 files_created.append(preprocess_cc_init())
 files_created.append(converter_cc_init())
 files_created.append(reader_cc_init())
 files_created.append(format_cc_init())
 files_created.append(object_cc_init())
+
+## Check whether the newly created folder is different from the existing one, and if it is, replace it
 for file_created in files_created:
     existing_file = file_created[:file_created.rfind(temp_suffix)]
     replace_old_file = True
