f60f2368d2eef9b5cb6529bfeda57750097cb763
ardasener <ardasenercs@gmail.com>
2022-03-14 09:44:04 +0300

    Added dimensions to the file header, moved endianness to the file header, added instantiations

---
+++ src/sparsebase/sparse_writer.cc
@@ -0,0 +1,45 @@
+#include "sparsebase/sparse_writer.h"
+#include "sparsebase/sparse_exception.h"
+#include "sparsebase/sparse_file_format.h"
+
+namespace sparsebase::utils {
+
+template <typename IDType, typename NNZType, typename ValueType>
+BinaryWriter<IDType, NNZType, ValueType>::BinaryWriter(std::string filename)
+    : filename_(filename) {}
+
+
+template <typename IDType, typename NNZType, typename ValueType>
+void BinaryWriter<IDType, NNZType, ValueType>::WriteCOO(
+    format::COO<IDType, NNZType, ValueType> *coo) const {
+
+  SbffObject sbff("coo");
+  sbff.AddArray("row", coo->get_row(), coo->get_num_nnz());
+  sbff.AddArray("col", coo->get_col(), coo->get_num_nnz());
+
+  if(coo->get_vals() != nullptr)
+    sbff.AddArray("vals", coo->get_vals(), coo->get_num_nnz());
+
+  sbff.Write(filename_);
+}
+
+template <typename IDType, typename NNZType, typename ValueType>
+void BinaryWriter<IDType, NNZType, ValueType>::WriteCSR(
+    format::CSR<IDType, NNZType, ValueType> *csr) const {
+
+  SbffObject sbff("csr");
+
+  int n,m;
+  auto dimensions = csr->get_dimensions();
+  n = dimensions[0];
+  m = dimensions[1];
+
+  sbff.AddArray("row_ptr", csr->get_row_ptr(), n+1);
+  sbff.AddArray("col", csr->get_col(), m);
+
+  if(csr->get_vals() != nullptr)
+    sbff.AddArray("vals", csr->get_vals(), m);
+
+  sbff.Write(filename_);
+}
+}
