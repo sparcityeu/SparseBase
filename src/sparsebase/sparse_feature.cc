3d1facc76d954694dc24bed642a8c099a845f264
Atahanak <aakyildiz@sabanciuniv.edu>
2022-03-29 13:21:16 +0300

    implementation and instantiation is seperated to Extractor and FeatureExtractor

--- src/sparsebase/sparse_feature.cc
+++ src/sparsebase/sparse_feature.cc
@@ -77,20 +77,7 @@ std::vector<ClassType> ClassMatcherMixin<ClassType, Key, KeyHash, KeyEqualTo>::
   return res;
 }
 
-template<typename IDType, typename NNZType, typename ValueType, typename FeatureType>
-Extractor<IDType, NNZType, ValueType, FeatureType>::Extractor(){
-
-  auto degree_distribution = new preprocess::DegreeDistribution<IDType, NNZType, ValueType, FeatureType>();
-  this->RegisterClass(degree_distribution->get_sub_ids(), degree_distribution);
-  auto degrees = new preprocess::Degrees<IDType, NNZType, ValueType>();
-  this->RegisterClass(degrees->get_sub_ids(), degrees);
-
-  auto degrees_degreedistribution = new preprocess::Degrees_DegreeDistribution<IDType, NNZType, ValueType, FeatureType>();
-  this->RegisterClass(degrees_degreedistribution->get_sub_ids(), degrees_degreedistribution);
-}
-
-template<typename IDType, typename NNZType, typename ValueType, typename FeatureType>
-void Extractor<IDType, NNZType, ValueType, FeatureType>::PrintFuncList() {
+void Extractor::PrintFuncList() {
   std::cout<< std::endl;
   std::cout << "Registered functions: " << std::endl;
   for(auto & cls : map_){
@@ -102,20 +89,17 @@ void Extractor<IDType, NNZType, ValueType, FeatureType>::PrintFuncList() {
   std::cout<< std::endl;
 }
 
-template<typename IDType, typename NNZType, typename ValueType, typename FeatureType>
-std::unordered_map<std::type_index, std::any> Extractor<IDType, NNZType, ValueType, FeatureType>::Extract(std::vector<Feature> & fs,
+std::unordered_map<std::type_index, std::any>Extractor::Extract(std::vector<Feature> & fs,
                                                                                    format::Format * format) {
     std::unordered_map<std::type_index, std::any> res;
-    for(auto & f : fs){
-      auto t = f->Extract(format);
+    for(auto & el : fs){
+      auto t = el->Extract(format);
       res.merge(t);
     }
     return res;
 }
 
-template<typename IDType, typename NNZType, typename ValueType, typename FeatureType>
-std::unordered_map<std::type_index, std::any> Extractor<IDType, NNZType, ValueType, FeatureType>::Extract(format::Format * f){
-
+std::unordered_map<std::type_index, std::any>Extractor::Extract(format::Format * format){
   if(in_.size() == 0)
     return {};
   // match and get classes for feature extraction
@@ -124,14 +108,13 @@ std::unordered_map<std::type_index, std::any> Extractor<IDType, NNZType, ValueTy
   std::cout << std::endl << "Classes used:" << std::endl;
   for(std::vector<preprocess::ExtractableType*>::iterator it = cs.begin(); it != cs.end(); it++){
     std::cout << (*it)->get_feature_id().name() << std::endl;
-    res.merge((*it)->Extract(f));
+    res.merge((*it)->Extract(format));
   }
   std::cout << std::endl;
   return res;
 }
 
-template<typename IDType, typename NNZType, typename ValueType, typename FeatureType>
-void Extractor<IDType, NNZType, ValueType, FeatureType>::Add(Feature f){
+void Extractor::Add(Feature f){
   if(map_.find(f->get_sub_ids()) != map_.end()){ //check if the class is registered
     for(auto & cls : f->get_subs()){
       auto id = cls->get_feature_id();
@@ -146,8 +129,7 @@ void Extractor<IDType, NNZType, ValueType, FeatureType>::Add(Feature f){
   }
 }
 
-template<typename IDType, typename NNZType, typename ValueType, typename FeatureType>
-void Extractor<IDType, NNZType, ValueType, FeatureType>::Substract(Feature f){
+void Extractor::Subtract(Feature f){
   for(auto id : f->get_sub_ids()){
     if(in_.find(id) != in_.end()){
       delete in_[id];
@@ -156,20 +138,30 @@ void Extractor<IDType, NNZType, ValueType, FeatureType>::Substract(Feature f){
   }
 }
 
-template<typename IDType, typename NNZType, typename ValueType, typename FeatureType>
-std::vector<std::type_index> Extractor<IDType, NNZType, ValueType, FeatureType>::GetList(){
+std::vector<std::type_index> Extractor::GetList(){
   std::vector<std::type_index> res;
   for(auto & el : in_){
     res.push_back(std::get<0>(el));
   }
   return res;
 }
-template <typename IDType, typename NNZType, typename ValueType,
-          typename FeatureType>
-Extractor<IDType, NNZType, ValueType, FeatureType>::~Extractor() {
+
+Extractor::~Extractor() {
   for(auto & el: in_){
     delete el.second;
   }
+}
+
+template <typename IDType, typename NNZType, typename ValueType,
+    typename FeatureType>
+FeatureExtractor<IDType, NNZType, ValueType, FeatureType>::FeatureExtractor() {
+  auto degree_distribution = new preprocess::DegreeDistribution<IDType, NNZType, ValueType, FeatureType>();
+  this->RegisterClass(degree_distribution->get_sub_ids(), degree_distribution);
+  auto degrees = new preprocess::Degrees<IDType, NNZType, ValueType>();
+  this->RegisterClass(degrees->get_sub_ids(), degrees);
+
+  auto degrees_degreedistribution = new preprocess::Degrees_DegreeDistribution<IDType, NNZType, ValueType, FeatureType>();
+  this->RegisterClass(degrees_degreedistribution->get_sub_ids(), degrees_degreedistribution);
 }
 
 #if !defined(_HEADER_ONLY)
