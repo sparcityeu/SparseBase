48d729740e7cc084fe16117f30073f7ae3d6afef
AmroAlJundi <amr.jundi@gmail.com>
2022-04-17 01:06:41 +0300

    Applied formatting to example source files

--- examples/array_example/array_example.cu
+++ examples/array_example/array_example.cu
@@ -1,46 +1,48 @@
 #include <iostream>
 
+#include "sparsebase/format/cuda/format.cuh"
 #include "sparsebase/format/format.h"
 #include "sparsebase/utils/converter/converter.h"
-#include "sparsebase/format/cuda/format.cuh"
 
 using namespace std;
 using namespace sparsebase;
 
-void print_array(int * vals, int n){
-    printf("Printing the Array on the CPU\n");
-    for (int i = 0; i < n; i++){
-        printf("%d ", vals[i]);
-    }
-    printf("\n");
+void print_array(int *vals, int n) {
+  printf("Printing the Array on the CPU\n");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", vals[i]);
+  }
+  printf("\n");
 }
-__global__ void print_array_cuda(int * vals, int n){
-    printf("Printing the Array on the GPU\n");
-    for (int i = 0; i < n; i++){
-        printf("%d ", vals[i]);
-    }
-    printf("\n");
-}
-
-int main(){
-
-    int vals[6] = {10, 20, 30, 40, 50, 60};
-    context::cuda::CUDAContext gpu_context{0};
-    context::CPUContext cpu_context;
-
-    format::Array<int>* array = new format::Array<int>(6, vals);
-
-    auto converter = new utils::converter::ConverterOrderOne<int>();
-
-    auto cuda_array = converter->Convert<format::cuda::CUDAArray<int>>(array, &gpu_context);
-
-    print_array_cuda<<<1,1>>>(cuda_array->get_vals(), cuda_array->get_dimensions()[0]);
-    cudaDeviceSynchronize();
-
-    auto cpu_array = converter->Convert<format::Array<int>>(cuda_array, &cpu_context);
-
-    print_array(cpu_array->get_vals(), cuda_array->get_dimensions()[0]);
-
-    return 0;
+__global__ void print_array_cuda(int *vals, int n) {
+  printf("Printing the Array on the GPU\n");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", vals[i]);
+  }
+  printf("\n");
 }
 
+int main() {
+
+  int vals[6] = {10, 20, 30, 40, 50, 60};
+  context::cuda::CUDAContext gpu_context{0};
+  context::CPUContext cpu_context;
+
+  format::Array<int> *array = new format::Array<int>(6, vals);
+
+  auto converter = new utils::converter::ConverterOrderOne<int>();
+
+  auto cuda_array =
+      converter->Convert<format::cuda::CUDAArray<int>>(array, &gpu_context);
+
+  print_array_cuda<<<1, 1>>>(cuda_array->get_vals(),
+                             cuda_array->get_dimensions()[0]);
+  cudaDeviceSynchronize();
+
+  auto cpu_array =
+      converter->Convert<format::Array<int>>(cuda_array, &cpu_context);
+
+  print_array(cpu_array->get_vals(), cuda_array->get_dimensions()[0]);
+
+  return 0;
+}
