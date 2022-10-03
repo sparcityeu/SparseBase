93ac8ba27c477081b09bbe0848c07359bdcb49f6
Atahanak <aakyildiz@sabanciuniv.edu>
2022-10-03 21:32:52 +0300

    spmv implemented

--- examples/example_experiment/example_experiment.cc
+++ examples/example_experiment/example_experiment.cc
@@ -11,39 +11,66 @@ using namespace format;
 using namespace preprocess;
 using namespace utils;
 using namespace io;
+using namespace context;
 
 using vertex_type = unsigned int;
 using edge_type = unsigned int;
 using value_type = unsigned int;
 
-std::any kernel_f2(unordered_map<string, Format *> data){
-  cout << "In function kernel_f2" << endl;
+#define NUM_RUNS 1
+
+std::any spmv(unordered_map<string, Format *> & data, std::any params){
+  auto v = any_cast<double*>(params);
+  auto spm = data["processed_format"]->As<CSR<vertex_type, edge_type, value_type>>();
+  auto dimensions = spm->get_dimensions();
+  auto num_rows = dimensions[0];
+  auto rows = spm->get_row_ptr();
+  auto cols = spm->get_col();
+
+  auto * res = new double[num_rows];
+  for(unsigned int i = 0; i < num_rows; i++){
+    double s = 0;
+    for(unsigned int j = rows[i]; j < rows[i+1]; j++){
+      auto idx = cols[j];
+      s += v[idx];
+    }
+    res[i] = s;
+  }
+
+  return res;
+}
+
+std::any spmv_gpu(unordered_map<string, Format *> & data, std::any params){
   std::any r;
   return r;
 }
 
 int main(int argc, char **argv){
 
-  experiment::ConcreteExperiment exp;
-  string file_name = argv[1];
-  exp.AddDataLoader(experiment::LoadCSR<CSR, MTXReader, vertex_type, edge_type, value_type>, {file_name});
-  exp.AddPreprocess(experiment::ReorderCSR<RCMReorder, vertex_type, edge_type, value_type>);
+  experiment::ConcreteExperiment cpu_exp;
+  for(unsigned int i = 1; i < argc; i++){
+    cpu_exp.AddDataLoader(experiment::LoadCSR<MTXReader, vertex_type, edge_type, value_type>, {argv[i]});
+  }
+  cpu_exp.AddPreprocess(experiment::Pass<vertex_type, edge_type, value_type>); // add dummy preprocessing to run kernels without preprocessing
+  cpu_exp.AddPreprocess(experiment::ReorderCSR<RCMReorder, CPUContext, vertex_type, edge_type, value_type>);
+  cpu_exp.AddPreprocess(experiment::Reorder<RCMReorder, CSR, CPUContext, vertex_type, edge_type, value_type>);
 
-  auto kernel_f = [] (unordered_map<string, Format*> data) {
-    cout << "DO WHATEVER YOU WANT WITH DATA!!!" << endl;
-    context::CPUContext cpu_context;
-    auto *perm = ReorderBase::Reorder<RCMReorder>({}, data["ordered"]->As<CSR<vertex_type, edge_type, value_type>>(), {&cpu_context}, true);
-    auto * A_reordered = ReorderBase::Permute2D<CSR>(perm, data["ordered"]->As<CSR<vertex_type, edge_type, value_type>>(), {&cpu_context}, true);
-    auto * A_csc = A_reordered->Convert<CSC>();
-    std::any rt = A_csc;
-    return rt;
-  };
-  exp.AddKernel(kernel_f);
-  exp.AddKernel(kernel_f2);
-  exp.Run(2);
-  std::vector<std::any> res = exp.GetResults();
-  cout << "Size = " << res.size() << endl;
-  auto secs = exp.GetRunTimes();
+  auto v = new double[958];
+  std::fill_n(v, 958, 1);
+  cpu_exp.AddKernel(spmv, v);
+
+  cpu_exp.Run(NUM_RUNS);
+
+  std::vector<std::any> res = cpu_exp.GetResults();
+  for(auto r: res){
+    auto result = any_cast<double*>(r);
+    for(unsigned int t = 0; t < 958; t++){
+      cout << result[958-t] << " ";
+    }
+    cout << endl;
+  }
+
+  auto secs = cpu_exp.GetRunTimes();
   for(auto s: secs){
     cout << "Run time: " << s << endl;
   }
