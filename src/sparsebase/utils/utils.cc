95819355d5db5899deef88ed73af183c45c943d0
Arda Sener <ardasenercs@gmail.com>
2022-10-20 23:52:22 +0300

    Moved some Logger code to the source file, changed names to fit google's style

--- src/sparsebase/utils/utils.cc
+++ src/sparsebase/utils/utils.cc
@@ -1,6 +1,9 @@
 #include "utils.h"
 #include "exception.h"
 #include <cxxabi.h>
+#include <algorithm>
+#include <sstream>
+#include <ctime>
 
 namespace sparsebase::utils {
     std::string demangle(const std::string& name) {
@@ -19,9 +22,55 @@ namespace sparsebase::utils {
       return demangle(type.name());
     }
 
-    LogLevel Logger::level = LOG_LVL_WARNING;
-    std::string Logger::filename;
-    bool Logger::use_stdout = true;
-    bool Logger::use_stderr = false;
+    Logger::Logger(std::type_index root_type) {
+      root_ = demangle(root_type);
+      root_.erase(std::remove(root_.begin(), root_.end(), '*'), root_.end());
+      if(!Logger::filename_.empty()) file_.open(filename_);
+    }
+
+    Logger::Logger() {
+      if(!Logger::filename_.empty()) file_.open(filename_);
+    }
+
+    Logger::~Logger(){
+      if(file_.is_open()) file_.close();
+    }
+
+    void Logger::Log(const std::string& message, LogLevel msg_level){
+
+      if(msg_level < Logger::level_){
+        return;
+      }
+
+      std::time_t current = std::time(0);
+      auto now_tm = std::localtime(&current);
+      char buffer[30];
+      size_t size = strftime(buffer, 30, "%x %X", now_tm);
+      std::string now_str(buffer, buffer + size);
+
+      std::stringstream ss;
+      ss << "[" << now_str << "]" << " ";
+
+      std::string level_str = "INFO";
+      if(msg_level == LOG_LVL_WARNING){
+        level_str = "WARNING";
+      }
+      ss << "[" << level_str << "]" << " ";
+
+      ss << "[" << root_ << "]" << " ";
+
+      ss << message;
+
+      std::string log = ss.str();
+
+      if(file_.is_open()) file_ << log << std::endl;
+      if(Logger::use_stdout_) std::cout << log << std::endl;
+      if(Logger::use_stderr_) std::cerr << log << std::endl;
+    }
+
+    LogLevel Logger::level_ = LOG_LVL_WARNING;
+    std::string Logger::filename_;
+    bool Logger::use_stdout_ = true;
+    bool Logger::use_stderr_ = false;
 }
 
