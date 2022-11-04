e7b801978c0a76ccbe328664eb318663cf77e989
AmroAlJundi <amr.jundi@gmail.com>
2022-11-04 08:03:42 +0000

    addressed reviewer comments + updated docs

--- src/sparsebase/preprocess/preprocess.cc
+++ src/sparsebase/preprocess/preprocess.cc
@@ -425,8 +425,12 @@ extern "C" {
 #endif
 
 template <typename IDType, typename NNZType, typename ValueType>
-AMDReorder<IDType, NNZType, ValueType>::AMDReorder(AMDReorderParams p){
+AMDReorder<IDType, NNZType, ValueType>::AMDReorder(AMDReorderParams p): AMDReorder(){
   this->params_ = std::make_unique<AMDReorderParams>(p);
+}
+template <typename IDType, typename NNZType, typename ValueType>
+AMDReorder<IDType, NNZType, ValueType>::AMDReorder(){
+  this->params_ = std::make_unique<AMDReorderParams>();
   this->RegisterFunction({format::CSR<IDType, NNZType, ValueType>::get_id_static()},
                          AMDReorderCSR);
 }
@@ -450,7 +454,7 @@ IDType* AMDReorder<IDType, NNZType, ValueType>::AMDReorderCSR(std::vector<format
   double *Info = nullptr; //new double[AMD_INFO]; // Auxiliary data
   int status = amd_l_order(n, xadj_long, adj_long, i_order, Control, Info);
   if (status != 0){
-    throw 1;
+    throw utils::ReorderException("AMD reordering failed");
   }
   IDType* order = new IDType[n];
   for (IDType i =0; i< n; i++) order[i_order[i]]=i;
@@ -1675,50 +1679,61 @@ IDType *MetisReorder<IDType, NNZType, ValueType>::GetReorderCSR(
 #endif
 
 template <typename IDType, typename NNZType, typename ValueType, typename FloatType>
-ReorderHeatmap<IDType, NNZType, ValueType, FloatType>::ReorderHeatmap(ReorderHeatmapParams params){
+ReorderHeatmap<IDType, NNZType, ValueType, FloatType>::ReorderHeatmap(){
   this->params_ =
-      std::make_unique<ReorderHeatmapParams>(params);
+      std::make_unique<ReorderHeatmapParams>();
   this->RegisterFunction(
-      {format::CSR<IDType, NNZType, ValueType>::get_id_static(), format::Array<IDType>::get_id_static()},
-      ReorderHeatmapCSRandArray);
-}
-template <typename IDType, typename NNZType, typename ValueType, typename FloatType>
-format::FormatOrderOne<FloatType>* ReorderHeatmap<IDType, NNZType, ValueType, FloatType>::Get(format::FormatOrderTwo<IDType, NNZType, ValueType> *format, format::FormatOrderOne<IDType>* permutation, std::vector<context::Context*> contexts, bool convert_input){
-  return this->Execute(this->params_.get(), contexts, convert_input, (format::Format*)format, (format::Format*)permutation);
+      {format::CSR<IDType, NNZType, ValueType>::get_id_static(), format::Array<IDType>::get_id_static(), format::Array<IDType>::get_id_static()},
+      ReorderHeatmapCSRArrayArray);
 }
 
 template <typename IDType, typename NNZType, typename ValueType, typename FloatType>
-format::FormatOrderOne<FloatType>* ReorderHeatmap<IDType, NNZType, ValueType, FloatType>::ReorderHeatmapCSRandArray(std::vector<format::Format*> formats, PreprocessParams* poly_params){
+ReorderHeatmap<IDType, NNZType, ValueType, FloatType>::ReorderHeatmap(ReorderHeatmapParams params) : ReorderHeatmap(){
+  this->params_ =
+      std::make_unique<ReorderHeatmapParams>(params);
+}
+
+template <typename IDType, typename NNZType, typename ValueType, typename FloatType>
+format::FormatOrderOne<FloatType>* ReorderHeatmap<IDType, NNZType, ValueType, FloatType>::Get(format::FormatOrderTwo<IDType, NNZType, ValueType> *format, format::FormatOrderOne<IDType>* permutation_r, format::FormatOrderOne<IDType>* permutation_c, std::vector<context::Context*> contexts, bool convert_input){
+  return this->Execute(this->params_.get(), contexts, convert_input, (format::Format*)format, (format::Format*)permutation_r,(format::Format*)permutation_c);
+}
+
+template <typename IDType, typename NNZType, typename ValueType, typename FloatType>
+format::FormatOrderOne<FloatType>* ReorderHeatmap<IDType, NNZType, ValueType, FloatType>::ReorderHeatmapCSRArrayArray(std::vector<format::Format*> formats, PreprocessParams* poly_params){
   auto csr = formats[0]->AsAbsolute<format::CSR<IDType, NNZType, ValueType>>();
-  auto order = formats[1]->AsAbsolute<format::Array<IDType>>()->get_vals();
+  auto order_r = formats[1]->AsAbsolute<format::Array<IDType>>()->get_vals();
+  auto order_c = formats[2]->AsAbsolute<format::Array<IDType>>()->get_vals();
   auto* params = static_cast<ReorderHeatmapParams*>(poly_params);
   int b = params->num_parts;
+  if (b > csr->get_dimensions()[0] || b > csr->get_dimensions()[1]) {
+    throw utils::ReorderException("Cannot generate heatmap for matrix when num_parts > number of rows or columns");
+  }
   auto n = csr->get_dimensions()[0];
   auto row_ptr = csr->get_row_ptr();
   auto adj = csr->get_col();
-  long long max_bw = 0;
-  double mean_bw = 0;
+  IDType max_bw = 0;
+  FloatType mean_bw = 0;
 
-  long long bsize = n / b;
+  IDType bsize = n / b;
 
   // matrix of size num_parts x num_parts with number of edges in each square
-  auto density = new long long*[b];
-  for(long long i = 0; i < b; i++) {
-    density[i] = new long long[b];
-    memset(density[i], 0, sizeof(long long) * b);
+  auto density = new NNZType*[b];
+  for(NNZType i = 0; i < b; i++) {
+    density[i] = new NNZType[b];
+    memset(density[i], 0, sizeof(NNZType) * b);
   }
 
-  for(long long i = 0; i < n; i++) {
-    long long u = order[i];
-    long long bu = u / bsize;
+  for(IDType i = 0; i < n; i++) {
+    IDType u = order_r[i];
+    IDType bu = u / bsize;
     if(bu == b) bu--;
-    for(long long ptr = row_ptr[i]; ptr < row_ptr[i+1]; ptr++) {
-      long long v = order[adj[ptr]];
-      long long bw = abs(u - v);
-      max_bw = std::max<long long>(max_bw, bw);
+    for(NNZType ptr = row_ptr[i]; ptr < row_ptr[i+1]; ptr++) {
+      IDType v = order_c[adj[ptr]];
+      IDType bw = abs(std::make_signed_t<IDType>(u) - std::make_signed_t<IDType>(v));
+      max_bw = std::max<IDType>(max_bw, bw);
       mean_bw += bw;
 
-      long long bv = v / bsize;
+      IDType bv = v / bsize;
       if(bv == b) bv--;
       density[bu][bv]++;
     }
@@ -1727,17 +1742,17 @@ format::FormatOrderOne<FloatType>* ReorderHeatmap<IDType, NNZType, ValueType, Fl
   //utils::Logger logger;
   //logger.Log("BW stats -- Mean bw: "+std::to_string(mean_bw) + "Max bw: " + std::to_string(max_bw), utils::LOG_LVL_INFO);
 
-  double para_mean_bw = 0;
+  FloatType para_mean_bw = 0;
   mean_bw = 0;
-  long long fblocks = 0;
+  int fblocks = 0;
   //logger.Log("Printing blocks \n----------------------------------------------" << endl;
-  for(long long i = 0; i < b; i++) {
-    for(long long j = 0; j < b; j++) {
+  for(int i = 0; i < b; i++) {
+    for(int j = 0; j < b; j++) {
       //     cout << std::setprecision(2) << density[i][j] / (row_ptr[n] + .0f) << "\t";
       if(density[i][j] > 0) {
         fblocks++;
       }
-      long long bw = std::abs(i - j);
+      int bw = std::abs(i - j);
       mean_bw += bw * density[i][j];
     }
     //   cout << endl;
--- tests/suites/sparsebase/preprocess/preprocess_tests.cc
+++ tests/suites/sparsebase/preprocess/preprocess_tests.cc
@@ -56,6 +56,12 @@ int rc_row_ptr[n + 1] = {0, 1, 3, 4};
 int rc_rows[nnz] = {0, 1, 1, 2};
 int rc_cols[nnz] = {2, 0, 1, 2};
 int rc_vals[nnz] = {4, 1, 2, 3};
+/*
+0 0 4
+1 2 0
+0 0 3
+*/
+float heatmap_rc_order_true[3*3] = {0, 0, 0.25, 0.25, 0.25, 0, 0, 0, 0.25};
 
 const int array_length = 3;
 int inverse_perm_array[array_length] = {2, 0, 1};
@@ -611,14 +617,50 @@ TEST(ReorderHeatmap, Instance){
   ReorderHeatmap<int, int, int, float> heatmapper(3); 
   int no_reoder_perm_arr[3] = {0,1,2};
   format::Array<int> no_reorder_perm(3, no_reoder_perm_arr, format::kNotOwned);
-  auto heatmap = heatmapper.Get(&global_csr, &no_reorder_perm , {global_csr.get_context()}, true);
+  auto heatmap = heatmapper.Get(&global_csr, &no_reorder_perm, &no_reorder_perm, {global_csr.get_context()}, true);
   auto heatmap_arr = heatmap->As<format::Array>()->get_vals();
   for (int i =0; i< 9 ; i++) EXPECT_EQ(heatmap_arr[i], heatmap_no_order_true[i]);
   #ifdef USE_CUDA
   // Try with a cuda array and COO
   context::cuda::CUDAContext g0{0};
   format::cuda::CUDAArray<int>* cuda_arr = no_reorder_perm.Convert<format::cuda::CUDAArray>(&g0);
-  heatmap = heatmapper.Get(&global_coo, cuda_arr , {global_csr.get_context()}, true);
+  heatmap = heatmapper.Get(&global_coo, cuda_arr, cuda_arr, {global_csr.get_context()}, true);
+  heatmap_arr = heatmap->As<format::Array>()->get_vals();
+  for (int i =0; i< 9 ; i++) EXPECT_EQ(heatmap_arr[i], heatmap_no_order_true[i]);
+  #endif
+}
+
+TEST(ReorderHeatmap, InstanceTwoReorders){
+  ReorderHeatmap<int, int, int, float> heatmapper(3); 
+  int no_reoder_perm_arr[3] = {0,1,2};
+  format::Array<int> r_reorder_perm(3, r_reorder_vector, format::kNotOwned);
+  format::Array<int> c_reorder_perm(3, c_reorder_vector, format::kNotOwned);
+  auto heatmap = heatmapper.Get(&global_csr, &r_reorder_perm, &c_reorder_perm, {global_csr.get_context()}, true);
+  auto heatmap_arr = heatmap->As<format::Array>()->get_vals();
+  for (int i =0; i< 9 ; i++) EXPECT_EQ(heatmap_arr[i], heatmap_rc_order_true[i]);
+  #ifdef USE_CUDA
+  // Try with a cuda array and COO
+  context::cuda::CUDAContext g0{0};
+  format::cuda::CUDAArray<int>* cuda_arr_r = r_reorder_perm.Convert<format::cuda::CUDAArray>(&g0);
+  format::cuda::CUDAArray<int>* cuda_arr_c = c_reorder_perm.Convert<format::cuda::CUDAArray>(&g0);
+  heatmap = heatmapper.Get(&global_coo, cuda_arr_r, cuda_arr_c, {global_csr.get_context()}, true);
+  heatmap_arr = heatmap->As<format::Array>()->get_vals();
+  for (int i =0; i< 9 ; i++) EXPECT_EQ(heatmap_arr[i], heatmap_rc_order_true[i]);
+  #endif
+}
+
+TEST(ReorderHeatmap, InstanceDefaultConstructor){
+  ReorderHeatmap<int, int, int, float> heatmapper; 
+  int no_reoder_perm_arr[3] = {0,1,2};
+  format::Array<int> no_reorder_perm(3, no_reoder_perm_arr, format::kNotOwned);
+  auto heatmap = heatmapper.Get(&global_csr, &no_reorder_perm, &no_reorder_perm,{global_csr.get_context()}, true);
+  auto heatmap_arr = heatmap->As<format::Array>()->get_vals();
+  for (int i =0; i< 9 ; i++) EXPECT_EQ(heatmap_arr[i], heatmap_no_order_true[i]);
+  #ifdef USE_CUDA
+  // Try with a cuda array and COO
+  context::cuda::CUDAContext g0{0};
+  format::cuda::CUDAArray<int>* cuda_arr = no_reorder_perm.Convert<format::cuda::CUDAArray>(&g0);
+  heatmap = heatmapper.Get(&global_coo, cuda_arr, cuda_arr, {global_csr.get_context()}, true);
   heatmap_arr = heatmap->As<format::Array>()->get_vals();
   for (int i =0; i< 9 ; i++) EXPECT_EQ(heatmap_arr[i], heatmap_no_order_true[i]);
   #endif
@@ -627,14 +669,30 @@ TEST(ReorderHeatmap, Instance){
 TEST(ReorderBase, Heatmap){
   int no_reoder_perm_arr[3] = {0,1,2};
   format::Array<int> no_reorder_perm(3, no_reoder_perm_arr, format::kNotOwned);
-  auto heatmap_arr = ReorderBase::Heatmap<float>(&global_csr, &no_reorder_perm, 3, {global_csr.get_context()}, true)->get_vals();
+  auto heatmap_arr = ReorderBase::Heatmap<float>(&global_csr, &no_reorder_perm, &no_reorder_perm, 3, {global_csr.get_context()}, true)->get_vals();
   for (int i =0; i< 9 ; i++) EXPECT_EQ(heatmap_arr[i], heatmap_no_order_true[i]);
   #ifdef USE_CUDA
   // Try with a cuda array and COO
   context::cuda::CUDAContext g0{0};
   format::cuda::CUDAArray<int>* cuda_arr = no_reorder_perm.Convert<format::cuda::CUDAArray>(&g0);
-  heatmap_arr =ReorderBase::Heatmap<float>(&global_coo, cuda_arr, 3, {global_csr.get_context()}, true)->get_vals();
+  heatmap_arr =ReorderBase::Heatmap<float>(&global_coo, cuda_arr, cuda_arr, 3, {global_csr.get_context()}, true)->get_vals();
   for (int i =0; i< 9 ; i++) EXPECT_EQ(heatmap_arr[i], heatmap_no_order_true[i]);
+  #endif
+}
+TEST(ReorderBase, HeatmapTwoReorders){
+  ReorderHeatmap<int, int, int, float> heatmapper(3); 
+  int no_reoder_perm_arr[3] = {0,1,2};
+  format::Array<int> r_reorder_perm(3, r_reorder_vector, format::kNotOwned);
+  format::Array<int> c_reorder_perm(3, c_reorder_vector, format::kNotOwned);
+  auto heatmap_arr = ReorderBase::Heatmap<float>(&global_csr, &r_reorder_perm, &c_reorder_perm, 3, {global_csr.get_context()}, true)->get_vals();
+  for (int i =0; i< 9 ; i++) EXPECT_EQ(heatmap_arr[i], heatmap_rc_order_true[i]);
+  #ifdef USE_CUDA
+  // Try with a cuda array and COO
+  context::cuda::CUDAContext g0{0};
+  format::cuda::CUDAArray<int>* cuda_arr_r = r_reorder_perm.Convert<format::cuda::CUDAArray>(&g0);
+  format::cuda::CUDAArray<int>* cuda_arr_c = c_reorder_perm.Convert<format::cuda::CUDAArray>(&g0);
+  heatmap_arr = ReorderBase::Heatmap<float>(&global_coo, cuda_arr_r, cuda_arr_c, 3, {global_csr.get_context()}, true)->get_vals();
+  for (int i =0; i< 9 ; i++) EXPECT_EQ(heatmap_arr[i], heatmap_rc_order_true[i]);
   #endif
 }
 
