a34000998915b1a30c07a33b476b796c758ce11e
denizumuteser <denizumuteser@yahoo.com.tr>
2022-11-08 02:06:36 +0300

    added instantiation generation from json file

--- src/generate_explicit_instantiations.py
+++ src/generate_explicit_instantiations.py
@@ -5,6 +5,7 @@ import sys
 import os
 import argparse
 import itertools
+import json
 
 parser = argparse.ArgumentParser(description="Generate explicit instantiations of SparseBase template classes")
 parser.add_argument('--id-types', nargs='+', type=str, help= "C++ data types used for variables storing IDs")
@@ -83,121 +84,30 @@ def gen_inst(template, filename, ifdef=None, folder=output_folder, exceptions=No
         out.close()
 
 
-# Format
-reset_file("format.inc")
-gen_inst("class CSR<$id_type, $nnz_type, $value_type>", "format.inc")
-gen_inst("class CSC<$id_type, $nnz_type, $value_type>", "format.inc")
-gen_inst("class COO<$id_type, $nnz_type, $value_type>", "format.inc")
-gen_inst("class Array<$value_type>", "format.inc", exceptions={"$value_type": ['void']})
-gen_inst("class Array<$nnz_type>", "format.inc", exceptions={"$nnz_type": ['void']})
-gen_inst("class Array<$id_type>", "format.inc", exceptions={"$id_type": ['void']})
-
-# Format (CUDA)
-reset_file("format.inc", cuda_output_folder)
-gen_inst("class CUDACSR<$id_type, $nnz_type, $value_type>", "format.inc",
-         ifdef="USE_CUDA", folder=cuda_output_folder)
-gen_inst("class CUDAArray<$value_type>", "format.inc",
-         ifdef="USE_CUDA", folder=cuda_output_folder, exceptions={"$value_type": ['void']})
-gen_inst("class CUDAArray<$id_type>", "format.inc",
-         ifdef="USE_CUDA", folder=cuda_output_folder, exceptions={"$value_type": ['void']})
-gen_inst("class CUDAArray<$nnz_type>", "format.inc",
-         ifdef="USE_CUDA", folder=cuda_output_folder, exceptions={"$value_type": ['void']})
-
-
-# Object
-reset_file("object.inc")
-gen_inst("class AbstractObject<$id_type, $nnz_type, $value_type>", "object.inc")
-gen_inst("class Graph<$id_type, $nnz_type, $value_type>", "object.inc")
-
-# Converter
-reset_file("converter.inc")
-gen_inst("class ConverterOrderOne<$value_type>", "converter.inc", exceptions={"$value_type": ['void']})
-gen_inst("class ConverterOrderOne<$id_type>", "converter.inc", exceptions={"$id_type": ['void']})
-gen_inst("class ConverterOrderOne<$nnz_type>", "converter.inc", exceptions={"$nnz_type": ['void']})
-gen_inst("class ConverterOrderTwo<$id_type, $nnz_type, $value_type>", "converter.inc")
-
-# Converter (CUDA)
-reset_file("converter.inc", folder=cuda_output_folder)
-return_type = "format::Format * "
-params = "(format::Format *source, context::Context*context)"
-order_two_functions = ['CsrCUDACsrConditionalFunction',
-                       'CUDACsrCsrConditionalFunction',
-                       'CUDACsrCUDACsrConditionalFunction']
-order_one_functions = ['CUDAArrayArrayConditionalFunction',
-                       'ArrayCUDAArrayConditionalFunction']
-
-for func in order_two_functions:
-    gen_inst(return_type + func + "<$id_type, $nnz_type, $value_type>" + params, "converter.inc",
-             ifdef="USE_CUDA", folder=cuda_output_folder)
-
-for func in order_one_functions:
-    gen_inst(return_type + func + "<$id_type>" + params, "converter.inc",
-         ifdef="USE_CUDA", folder=cuda_output_folder, exceptions={"$id_type": ['void']})
-    gen_inst(return_type + func + "<$nnz_type>" + params, "converter.inc",
-         ifdef="USE_CUDA", folder=cuda_output_folder, exceptions={"$nnz_type": ['void']})
-    gen_inst(return_type + func + "<$value_type>" + params, "converter.inc",
-         ifdef="USE_CUDA", folder=cuda_output_folder, exceptions={"$value_type": ['void']})
-
-
-
-# Reader
-reset_file("reader.inc")
-gen_inst("class EdgeListReader<$id_type, $nnz_type, $value_type>", "reader.inc")
-gen_inst("class MTXReader<$id_type, $nnz_type, $value_type>", "reader.inc")
-gen_inst("class PigoEdgeListReader<$id_type, $nnz_type, $value_type>", "reader.inc")
-gen_inst("class PigoMTXReader<$id_type, $nnz_type, $value_type>", "reader.inc")
-gen_inst("class BinaryReaderOrderTwo<$id_type, $nnz_type, $value_type>", "reader.inc")
-gen_inst("class BinaryReaderOrderOne<$id_type>", "reader.inc", exceptions={"$id_type": ['void']})
-gen_inst("class BinaryReaderOrderOne<$nnz_type>", "reader.inc", exceptions={"$nnz_type": ['void']})
-gen_inst("class BinaryReaderOrderOne<$value_type>", "reader.inc", exceptions={"$value_type": ['void']})
-
-# Writer
-reset_file("writer.inc")
-gen_inst("class BinaryWriterOrderOne<$id_type>", "writer.inc", exceptions={"$id_type": ['void']})
-gen_inst("class BinaryWriterOrderOne<$nnz_type>", "writer.inc", exceptions={"$nnz_type": ['void']})
-gen_inst("class BinaryWriterOrderOne<$value_type>", "writer.inc", exceptions={"$value_type": ['void']})
-gen_inst("class BinaryWriterOrderTwo<$id_type, $nnz_type, $value_type>", "writer.inc")
-
-# Feature
-reset_file("feature.inc")
-gen_inst("class FeatureExtractor<$id_type, $nnz_type, $value_type, $float_type>", "feature.inc")
-
-# Preprocess
-reset_file("preprocess.inc")
-gen_inst("class Degrees<$id_type, $nnz_type, $value_type>", "preprocess.inc")
-gen_inst("class RCMReorder<$id_type, $nnz_type, $value_type>", "preprocess.inc")
-gen_inst("class DegreeReorder<$id_type, $nnz_type, $value_type>", "preprocess.inc")
-gen_inst("class GrayReorder<$id_type, $nnz_type, $value_type>", "preprocess.inc")
-gen_inst("class GenericReorder<$id_type, $nnz_type, $value_type>", "preprocess.inc")
-gen_inst("class RabbitReorder<$id_type, $nnz_type, $value_type>", "preprocess.inc", ifdef="USE_RABBIT_ORDER")
-gen_inst("class MetisReorder<$id_type, $nnz_type, $value_type>", "preprocess.inc", ifdef="USE_METIS")
-gen_inst("class AMDReorder<$id_type, $nnz_type, $value_type>", "preprocess.inc", ifdef="USE_AMD_ORDER")
-gen_inst("class ReorderHeatmap<$id_type, $nnz_type, $value_type, $float_type>", "preprocess.inc")
-gen_inst("class PermuteOrderTwo<$id_type, $nnz_type, $value_type>", "preprocess.inc")
-gen_inst("class PermuteOrderOne<$id_type, $value_type>", "preprocess.inc", exceptions={"$value_type": ['void']})
-gen_inst("class TransformPreprocessType<format::FormatOrderTwo<$id_type, $nnz_type, $value_type>, format::FormatOrderTwo<$id_type, $nnz_type, $value_type>>", "preprocess.inc")
-gen_inst("class TransformPreprocessType<format::FormatOrderOne<$value_type>, format::FormatOrderOne<$value_type>>", "preprocess.inc")
-gen_inst("class TransformPreprocessType<format::FormatOrderOne<$id_type>, format::FormatOrderOne<$id_type>>", "preprocess.inc")
-gen_inst("class TransformPreprocessType<format::FormatOrderOne<$nnz_type>, format::FormatOrderOne<$nnz_type>>", "preprocess.inc")
-gen_inst("class DegreeDistribution<$id_type, $nnz_type, $value_type, $float_type>", "preprocess.inc")
-gen_inst("class Degrees_DegreeDistribution<$id_type, $nnz_type, $value_type, $float_type>", "preprocess.inc")
-gen_inst("class JaccardWeights<$id_type, $nnz_type, $value_type, $float_type>", "preprocess.inc")
-gen_inst("class MetisPartition<$id_type, $nnz_type, $value_type>", "preprocess.inc", ifdef="USE_METIS")
-gen_inst("class PulpPartition<$id_type, $nnz_type, $value_type>", "preprocess.inc", ifdef="USE_PULP")
-gen_inst("class PatohPartition<$id_type, $nnz_type, $value_type>", "preprocess.inc", ifdef="USE_PATOH")
-gen_inst("class FunctionMatcherMixin<format::Format*>", "preprocess.inc")
-gen_inst("class FunctionMatcherMixin<$id_type*>", "preprocess.inc")
-gen_inst("class FunctionMatcherMixin<$id_type>", "preprocess.inc")
-gen_inst("class FunctionMatcherMixin<$id_type*, ExtractableType>", "preprocess.inc")
-gen_inst("class FunctionMatcherMixin<$id_type, ExtractableType>", "preprocess.inc")
-gen_inst("class GenericPreprocessType<$id_type>", "preprocess.inc")
-gen_inst("class ReorderPreprocessType<$id_type>", "preprocess.inc")
-gen_inst("class PartitionPreprocessType<$id_type>", "preprocess.inc")
-
-# Preprocess (CUDA)
-reset_file("preprocess.inc", cuda_output_folder)
-gen_inst("format::cuda::CUDAArray<$float_type>* "
-         + "RunJaccardKernel<$id_type, $nnz_type, $value_type, $float_type>(format::cuda::CUDACSR<$id_type, $nnz_type, $value_type>*)",
-         "preprocess.inc",
-         ifdef="USE_CUDA",
-         folder=cuda_output_folder)
+#reading class registration file
+with open('/mnt/d/Repos/sparsebase/src/class_instantiation_list.json', 'r') as read_file:
+    data = json.load(read_file)
+    
+current_filename = ""
+class_instantiation_list = data.get("classes")
+for each_class in class_instantiation_list:
+    #print(each_class)
+    #setting current filename
+    if current_filename != each_class.get("filename"):
+        current_filename = each_class.get("filename")
+        reset_file(current_filename)
+    
+    #choosing folder
+    current_folder = each_class.get("folder")
+    if each_class.get("ifdef") == "USE_CUDA":
+        current_folder = cuda_output_folder
+    elif each_class.get("folder") == None:
+        current_folder = output_folder  
+        
+    gen_inst(
+        template=each_class.get("template"),
+        filename=current_filename,
+        ifdef=each_class.get("ifdef"),
+        folder=current_folder,
+        exceptions=each_class.get("exceptions")
+        )
