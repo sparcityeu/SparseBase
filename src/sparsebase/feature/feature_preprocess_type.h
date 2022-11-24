c0a3ee4ee677569f86e85dec53319689c06c6176
AmroAlJundi <amr.jundi@gmail.com>
2022-11-24 07:58:38 +0300

    Moved feature_preprocess_type to feature namespace
    # Conflicts:
    #	src/CMakeLists.txt
    #	src/generate_explicit_instantiations.py

---
+++ src/sparsebase/feature/feature_preprocess_type.h
@@ -0,0 +1,33 @@
+#include <memory>
+#include "sparsebase/utils/parameterizable.h"
+#include "sparsebase/utils/function_matcher_mixin.h"
+#include "sparsebase/utils/extractable.h"
+#ifndef SPARSEBASE_PROJECT_FEATURE_PREPROCESS_TYPE_H
+#define SPARSEBASE_PROJECT_FEATURE_PREPROCESS_TYPE_H
+namespace sparsebase::feature {
+//! A class that does feature extraction.
+/*!
+ * An Extractable class that has a function matching
+ * capability. In other words, an Extractable to which implementation functions
+ * can be added and used. @tparam FeatureType the return type of feature
+ * extraction
+ */
+template <typename FeatureType>
+class FeaturePreprocessType
+    : public utils::FunctionMatcherMixin<FeatureType,
+        utils::Extractable> {
+ public:
+  std::shared_ptr<utils::Parameters> get_params() override;
+  std::shared_ptr<utils::Parameters> get_params(std::type_index) override;
+  void set_params(std::type_index, std::shared_ptr<utils::Parameters>) override;
+  std::type_index get_id() override;
+  ~FeaturePreprocessType();
+};
+
+}
+
+#ifdef _HEADER_ONLY
+#include "sparsebase/feature/feature_preprocess_type.cc"
+#endif
+
+#endif  // SPARSEBASE_PROJECT_FEATURE_PREPROCESS_TYPE_H
--- src/sparsebase/preprocess/preprocess.cc
+++ src/sparsebase/preprocess/preprocess.cc
@@ -38,39 +38,6 @@ namespace sparsebase {
 namespace preprocess {
 
 
-
-template <typename FeatureType>
-FeaturePreprocessType<FeatureType>::~FeaturePreprocessType() = default;
-
-template <typename FeatureType>
-std::shared_ptr<utils::Parameters>
-FeaturePreprocessType<FeatureType>::get_params() {
-  return this->params_;
-}
-template <typename FeatureType>
-std::shared_ptr<utils::Parameters>
-FeaturePreprocessType<FeatureType>::get_params(std::type_index t) {
-  if (this->pmap_.find(t) != this->pmap_.end()) {
-    return this->pmap_[t];
-  } else {
-    throw utils::FeatureParamsException(get_id().name(), t.name());
-  }
-}
-template <typename FeatureType>
-void FeaturePreprocessType<FeatureType>::set_params(
-    std::type_index t, std::shared_ptr<utils::Parameters> p) {
-  auto ids = this->get_sub_ids();
-  if (std::find(ids.begin(), ids.end(), t) != ids.end()) {
-    this->pmap_[t] = p;
-  } else {
-    throw utils::FeatureParamsException(get_id().name(), t.name());
-  }
-}
-template <typename FeatureType>
-std::type_index FeaturePreprocessType<FeatureType>::get_id() {
-  return typeid(*this);
-}
-
 template <typename IDType, typename NNZType, typename ValueType,
           typename FeatureType>
 JaccardWeights<IDType, NNZType, ValueType, FeatureType>::JaccardWeights(
