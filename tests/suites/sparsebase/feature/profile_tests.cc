85e305afb0cfe9e278760869f8a2d399f90ae417
EnverAtahan <enveratahan@sabanciuniv.edu>
2023-05-24 10:18:27 +0300

    Implemented OffDiagBlockNNZ and Profile Features

---
+++ tests/suites/sparsebase/feature/profile_tests.cc
@@ -0,0 +1,87 @@
+#include <memory>
+#include <typeindex>
+#include <typeinfo>
+#include <utility>
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "sparsebase/bases/reorder_base.h"
+#include "sparsebase/context/context.h"
+#include "sparsebase/feature/profile.h"
+#include "sparsebase/format/coo.h"
+#include "sparsebase/format/csc.h"
+#include "sparsebase/format/csr.h"
+#include "sparsebase/format/format_order_one.h"
+#include "sparsebase/format/format_order_two.h"
+#include "sparsebase/reorder/degree_reorder.h"
+#include "sparsebase/reorder/reorderer.h"
+#include "sparsebase/utils/exception.h"
+
+using namespace sparsebase;
+using namespace sparsebase::reorder;
+using namespace sparsebase::bases;
+using namespace sparsebase::feature;
+#include "../functionality_common.inc"
+
+class ProfileTest : public ::testing::Test {
+ protected:
+  feature::Profile<int, int, void> feature;
+  
+  struct Params1 : sparsebase::utils::Parameters {};
+};
+
+TEST_F(ProfileTest, AllTests) {
+  Params1 p1;
+  const int n = 4;
+  const int nnz = 7;
+  int row_ptr_[n+1]{0, 3, 4, 6, 7};
+  int col_[nnz]{0, 2, 3, 2, 4, 0, 2};
+  /*  1 0 1 1 0
+   *  0 0 1 0 0
+   *  1 0 0 0 1
+   *  0 0 1 0 0
+   * */
+  int64_t ans = 3;
+  auto csr = new sparsebase::format::CSR<int, int, void>(n, 5, row_ptr_, col_, nullptr,
+                                                                        sparsebase::format::kOwned);
+  // test get_sub_ids
+  EXPECT_EQ(feature.get_sub_ids().size(), 1);
+  EXPECT_EQ(feature.get_sub_ids()[0], std::type_index(typeid(feature)));
+
+  // Test get_subs
+  auto subs = feature.get_subs();
+  // a single sub-feature
+  EXPECT_EQ(subs.size(), 1);
+  // same type as feature but different address
+  auto &feat = *(subs[0]);
+  EXPECT_EQ(std::type_index(typeid(feat)), std::type_index(typeid(feature)));
+  EXPECT_NE(subs[0], &feature);
+
+  // Check GetProfileCSR implementation function
+  int64_t* profile =
+      feature::Profile<int, int, void>::GetProfileCSR({csr}, &p1);
+
+  EXPECT_EQ(*profile, ans);
+  delete profile;
+
+  // Check GetProfile
+  profile = feature.GetProfile(csr, {&cpu_context}, true);
+  EXPECT_EQ(*profile, ans);
+  delete profile;
+
+  profile = feature.GetProfile(csr, {&cpu_context}, false);
+  EXPECT_EQ(*profile, ans);
+  delete profile;
+
+  // Check Extract
+  auto feature_map = feature.Extract(csr, {&cpu_context}, true);
+  // Check map size and type
+  EXPECT_EQ(feature_map.size(), 1);
+  for (auto feat : feature_map) {
+    EXPECT_EQ(feat.first, std::type_index(typeid(feature)));
+  }
+
+  EXPECT_EQ(*std::any_cast<int64_t *>(feature_map[feature.get_id()]), ans);
+
+  delete profile;
+}
