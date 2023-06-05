afb3c7b4bf1e93ce183095d3cc44849329587de7
denizumuteser <denizumuteser@yahoo.com.tr>
2023-06-05 14:29:32 +0300

    added detailed comments to mtx_writer.cc

--- src/sparsebase/io/mtx_writer.cc
+++ src/sparsebase/io/mtx_writer.cc
@@ -20,11 +20,21 @@ MTXWriter<IDType, NNZType, ValueType>::MTXWriter(
       field_(field),
       symmetry_(symmetry) {}
 
+
+// This function does:
+// 1- Take input matrix as COO
+// 2- Convert it into MTX format
+// 3-Write it as .mtx file
 template <typename IDType, typename NNZType, typename ValueType>
 void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
     format::COO<IDType, NNZType, ValueType> *coo) const {
       
-      //illegal parameter checks
+      
+
+      //information about MTX (Matrix Market Format) https://math.nist.gov/MatrixMarket/formats.html and https://networkrepository.com/mtx-matrix-market-format.html
+      
+      //Matrix market format has some header parameters, following if conditions check these input parameters and 
+      //throws exception if its wrong, or illegal combination with other parameters or its not supported with sparsebase
       if (object_ != "matrix" && object_ != "vector") 
       {
         throw utils::WriterException("Illegal value for the 'object' option in matrix market header");
@@ -58,45 +68,54 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
         throw utils::WriterException("Matrix market writer does not currently support hermitian symmetry.");
       }
 
+      // open a mtx file to desired filename
       std::ofstream mtxFile;
       mtxFile.open(filename_);
       
-      //header
+      //insert the mtx headers which are inputted from user
       mtxFile << "%%MatrixMarket " << object_ << " " << format_ << " " << field_ << " " << symmetry_ << "\n";
       
       //TODO add warning when given field and actual data format is not same (integer vs float etc)
-
+      
+      //special condition to check if value type of matrix is void or not, MTX format supports void values if it has pattern parameter
       if constexpr (std::is_same_v<ValueType, void>)
       {
         if (field_ != "pattern")
           throw utils::WriterException("Cannot write an MTX with void ValueType, unless field is pattern.");
       }
 
+      //consruct dimensions of the matrix
       auto dimensions = coo->get_dimensions();
       IDType* row = coo->get_row();
       IDType* col = coo->get_col();
       ValueType* val = coo->get_vals();
 
+      //when pattern is selected no need to give values
       if (val != NULL && field_ == "pattern")
       {
         utils::Logger logger(typeid(this));
         logger.Log("Pattern selected but values given.", utils::LOG_LVL_WARNING);
       }
 
-      //Symmetry check
+      //check matrix Symmetry, user inputs this as parameter but we check it anyways to detect if any user error happens.
+      //also if skew symmetric or hermitian symmetric is choosen we change the matrix accordingly here before writing to file.
       bool saidSymmetric = (symmetry_ == "symmetric" || symmetry_ == "skew-symmetric" || symmetry_ == "hermitian");
       int NNZ = coo->get_num_nnz();
       int count_symmetric = 0;
       int count_diagonal = 0;
       int is_diagonal_all_zero = true;
+      //check symmetry
+      //saidSymmetric: user said symmetric (but it might not be)
       if (saidSymmetric == true && dimensions[0] != dimensions[1])
       {
         throw utils::WriterException("Matrix is not symmetric!");
       }
+      //easier symmetry check first, it needs to be square to be symmetric
       if (saidSymmetric && dimensions[0] == dimensions[1]) 
       {
           for (int i = 0; i < coo->get_num_nnz(); ++i) 
           {
+            //checking every non diagonal entry and their symmetric coordinate and compare values
             if (row[i] != col[i]) 
             { // Non-diagonal entry, check symmetric counterpart
                 bool found_symmetric = false;
@@ -104,6 +123,7 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
                 {
                   if (symmetry_ == "skew-symmetric")
                   {
+                    //thse is_same_v checks are to ensure function works even if value type is void becuase of pattern parameter
                     if constexpr (std::is_same_v<ValueType, void>)
                     {
                       break; //pattern cannot be skew-symmetric
@@ -120,6 +140,7 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
                   }
                   else
                   {
+                    //thse is_same_v checks are to ensure function works even if value type is void becuase of pattern parameter
                     if constexpr (std::is_same_v<ValueType, void>)
                     {
                       if (row[j] == col[i] && col[j] == row[i]) 
@@ -140,6 +161,7 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
                     }
                   }
                 }
+                //found_symmetric is true if matrix is actually symmetric (user might said not symmetric)
                 if (!found_symmetric) {
                     throw utils::WriterException("Matrix is not symmetric!");
                 }
@@ -147,6 +169,7 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
             else //diagonal
             {
               count_diagonal++;
+              //thse is_same_v checks are to ensure function works even if value type is void becuase of pattern parameter
               if constexpr (std::is_same_v<ValueType, void>)
               {
                 //zero
@@ -155,11 +178,13 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
               {
                 if (val[i] != 0)
                 {
+                  //checking this because we need this for skew-symmetry check
                   is_diagonal_all_zero = false;
                 }
               }
             }
           }
+          //check if matrix is actually skew-symmetric
           if(symmetry_ == "skew-symmetric")
           {
             if (!is_diagonal_all_zero)
@@ -175,6 +200,7 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
       }
 
       //dimensions and nnz
+      //if array is given rather than coordinate, dimension format of the file is different, so it is handled here
       if (format_ == "array")
       {
         mtxFile << dimensions[0] << " " << dimensions[1] << "\n";
@@ -183,6 +209,7 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
       {
         mtxFile << dimensions[0] << " " << dimensions[1] << " " << NNZ << "\n";
       }
+      //data lines are written here for array
       if (format_ == "array")
       {
         //write data lines
@@ -231,6 +258,7 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
             }
         }
       }
+      //data lines for coordinate are written here
       else //coordinate
       {
 
@@ -241,6 +269,7 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
             if (symmetry_ != "skew-symmetric" && col[i] == row[i])
             {//on diagonal entries
               mtxFile << row[i]+1 << " " << col[i]+1;
+              //thse is_same_v checks are to ensure function works even if value type is void becuase of pattern parameter
               if constexpr (std::is_same_v<ValueType, void>)
               {
                 if (field_ == "pattern")
@@ -261,6 +290,7 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
             if (col[i] < row[i])
             {//strictly below diagonal entries
               mtxFile << row[i]+1 << " " << col[i]+1;
+              //thse is_same_v checks are to ensure function works even if value type is void becuase of pattern parameter
               if constexpr (std::is_same_v<ValueType, void>)
               {
                 if (field_ == "pattern")
@@ -280,12 +310,13 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
             }
           }       
         }
+        //data lines for general format is written here
         else //general
         { 
           for (int i = 0; i < coo->get_num_nnz(); i++)
           {
             mtxFile << row[i]+1 << " " << col[i]+1;
-
+            //thse is_same_v checks are to ensure function works even if value type is void becuase of pattern parameter
             if constexpr (std::is_same_v<ValueType, void>)
             {
               if (field_ == "pattern")
@@ -311,7 +342,9 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCOO(
 template <typename IDType, typename NNZType, typename ValueType>
 void MTXWriter<IDType, NNZType, ValueType>::WriteCSR(
     format::CSR<IDType, NNZType, ValueType> *csr) const {
-
+      //this is for writing mtx file from CSR rather than COO
+      //this function doesnt convert CSR to MTX, it rather converts CSR to COO using sparsebase conversion
+      //then calls above WriteCOO function with this converted COO
       converter::ConverterOrderTwo<IDType, NNZType, ValueType> converterObj;
       context::CPUContext cpu_context;
       auto coo = converterObj.template Convert<format::COO<IDType, NNZType, ValueType>>(
@@ -322,6 +355,8 @@ void MTXWriter<IDType, NNZType, ValueType>::WriteCSR(
 template <typename IDType, typename NNZType, typename ValueType>
 void MTXWriter<IDType, NNZType, ValueType>::WriteArray(
   format::Array<ValueType> *arr) const {
+    //this function takes array as input and writes it strictly as array format
+    //it is same as WriteCOO otherwise    
     
     //illegal parameter checks
     if (object_ != "matrix" && object_ != "vector") 
